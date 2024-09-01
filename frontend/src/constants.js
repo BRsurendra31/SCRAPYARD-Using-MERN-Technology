c1a2b2c24525293598053d9b0c19fc838f762002
SURENDRA KUMAR <132610197+BRsurendra31@users.noreply.github.com>
2024-09-01 19:27:36 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,4 @@
-const API_URL = process.env.NODE_ENV !== 'development' ?
-    process.env.REACT_APP_BASE_URL :
-    'http://localhost:4000'
+const API_URL = process.env.REACT_APP_BASE_URL;
 
 console.log(process.env, "API_URL");
 
